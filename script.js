7571141b064ef57d46193c0eb9052c9048b859d7
OsipovaNadezda <osipavanadezda@gmail.com>
2022-06-21 20:32:35 +0300

    completed task№1 lesson06

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-//task 1
-console.log("Задание 1");
-let arr = ["12345", "234567", "345678", "45678", "56789", "456", "7890"];
+let random;
+let enteredNumber;
+let randomNumber;
 
-for (let i = 0; i < 7; i++) {
-    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-        console.log(arr[i]);
-    }
-}
+function randomInteger(min, max) {
+  random = min + Math.random() * (max - min + 1);
+  return Math.floor(random);
+};
 
-//task2
-console.log("Задание 2");
-const n = 100;
-for (let i = 1; i <= n; i++) {
-  for (let j = 2; j <= i; j++) {
-    if (i % j === 0 && j < i) {
-      break;
-    } else if (j === i) {
-      console.log(i + ": " + "Делители этого числа: 1 и " + i);
-    }
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+function qame() {
+  enteredNumber = prompt("Угадай число от 1 до 100", "Введи число от 1 до 100");
+  if (enteredNumber === null) {
+    alert('Игра окончена!');
+    return;
+  }
+  if (!isNumber(enteredNumber)) {
+    alert("Введи число!");
+    return game();
+  }
+
+  enteredNumber = +enteredNumber;
+
+  if (enteredNumber > randomNumber) {
+    alert("Загаданное число меньше");
+    return qame();
+  } else if (enteredNumber < randomNumber) {
+    alert("Загаданное число больше");
+    return qame();
+  } else if (enteredNumber === randomNumber) {
+    alert("Поздравляю, Вы угадали!!!");
   }
 }
+
+randomNumber = randomInteger(1, 10);
+qame();
+
+console.log(enteredNumber);
+console.log(randomNumber);
