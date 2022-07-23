dcd7539daaa8865692cbea2ef24ded3f74276a2d
OsipovaNadezda <osipavanadezda@gmail.com>
2022-07-23 10:11:39 +0300

    complited task№2 lesson19

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-let random;
-let enteredNumber;
-let randomNumber;
+'use strict'
+const deadline = "01 January 2023";
+const timeOfDay = document.getElementById('time-of-day');
+const dayOfWeek = document.getElementById('day-of-week');
+const curentTime = document.getElementById('curent-time');
+const timeNewYears = document.getElementById('time-new-years');
 
-function randomInteger(min, max) {
-  random = min + Math.random() * (max - min + 1);
-  return Math.floor(random);
+let date = new Date();
+let dateNewYears = new Date(deadline).getTime();
+let timeBeforeNewYear = (dateNewYears - date) / 1000;
+
+const getTimeOfDay = () => {
+
+  if (date.getHours() >= 0 && date.getHours() < 6) {
+    return "ой ночи!";
+  }
+  if (date.getHours() >= 6 && date.getHours() < 12) {
+    return "ое утро!";
+  }
+  if (date.getHours() >= 12 && date.getHours() < 18) {
+    return "ый день!";
+  }
+  if (date.getHours() >= 18 && date.getHours() < 24) {
+    return "ый вечер!";
+  }
 };
 
-const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
+const getDayOfWeek = () => {
+  const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+  let today = date.getDay();
+  return week[today - 1];
 };
 
-function qame() {
-  enteredNumber = prompt("Угадай число от 1 до 100", "Введи число от 1 до 100");
-  if (enteredNumber === null) {
-    alert('Игра окончена!');
-    return;
-  }
-  if (!isNumber(enteredNumber)) {
-    alert("Введи число!");
-    return game();
-  }
+const getCurentTime = () => {
+  return date.toLocaleTimeString('en');
+};
 
-  enteredNumber = +enteredNumber;
+const getTimeNewYears = () => {
+  let dateNewYears = new Date(deadline).getTime();
+  let timeBeforeNewYear = (dateNewYears - date) / 1000;
 
-  if (enteredNumber > randomNumber) {
-    alert("Загаданное число меньше");
-    return qame();
-  } else if (enteredNumber < randomNumber) {
-    alert("Загаданное число больше");
-    return qame();
-  } else if (enteredNumber === randomNumber) {
-    alert("Поздравляю, Вы угадали!!!");
+  let days = Math.floor((timeBeforeNewYear / 60 / 60) / 24);
+  let days100 = days % 100;
+  let days10 = days100 % 10;
+
+  if (days === 1 || days10 === 1) {
+    return days + ' день';
+  } else if (days > 1 && days < 5 || days10 > 1 && days10 < 5) {
+    return days + ' дня';
+  } else {
+    return days + ' дней';
   }
 }
 
-randomNumber = randomInteger(1, 10);
-qame();
+const resultOutput = () => {
+  timeOfDay.innerText = getTimeOfDay();
+  dayOfWeek.innerText = getDayOfWeek();
+  curentTime.innerText = getCurentTime();
+  timeNewYears.innerText = getTimeNewYears();
+};
 
-console.log(enteredNumber);
-console.log(randomNumber);
+resultOutput();
